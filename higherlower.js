57a9766f763f4b8d83c809c76386beb8556175e7
Bryan W <bhoff1980@gmail.com>
2023-04-12 16:09:17 -0600

    assessment meets all given creiteria

--- higherlower.js
+++ higherlower.js
@@ -2,7 +2,7 @@ const guessAction = document.getElementById("guess-btn");
 const enteredNum = document.getElementById("entered-num");
 let choice = prompt("Please enter the largest number for the game");
 let isNumber = /^\d+$/.test(choice);
-
+let allGuess = [];
 let rounded = Math.round(choice);
 
 if (rounded < 1 || isNumber === false) {
@@ -16,13 +16,24 @@ let num = Math.floor(Math.random() * rounded) + 1;
 function do_guess() {
   let guess = Number(document.getElementById("guess").value);
   let message = document.getElementById("message");
+
+  for (let i = 0; i < allGuess.length; i++) {
+    if (guess === allGuess[i]) {
+      message.innerHTML = "That number has already been guessed, try again.";
+      return;
+    }
+  }
+
   if (guess < 1 || guess > rounded) {
     message.innerHTML = "That number is not in range, try again.";
   } else if (num === guess) {
-    message.innerHTML = "You got it!";
+    allGuess.push(guess);
+    message.innerHTML = `You got it! It took you ${allGuess.length} tries and your guesses were ${allGuess.join(", ")}.`;
   } else if (num < guess) {
+    allGuess.push(guess);
     message.innerHTML = "No try a lower number";
   } else if (num > guess) {
+    allGuess.push(guess);
     message.innerHTML = "No try a higher number";
   } else {
     message.innerHTML = "That is not a number!";
