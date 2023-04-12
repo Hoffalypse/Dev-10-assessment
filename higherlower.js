c00e257cae42730fa37bba431d925cd55f243292
Bryan W <bhoff1980@gmail.com>
2023-04-12 15:42:16 -0600

    guess validation completed

--- higherlower.js
+++ higherlower.js
@@ -5,7 +5,6 @@ let isNumber = /^\d+$/.test(choice);
 
 let rounded = Math.round(choice);
 
-
 if (rounded < 1 || isNumber === false) {
   alert("Please Enter a Positive Number");
   document.location.reload();
@@ -17,13 +16,16 @@ let num = Math.floor(Math.random() * rounded) + 1;
 function do_guess() {
   let guess = Number(document.getElementById("guess").value);
   let message = document.getElementById("message");
-
-  if (num === guess) {
+  if (guess < 1 || guess > rounded) {
+    message.innerHTML = "That number is not in range, try again.";
+  } else if (num === guess) {
     message.innerHTML = "You got it!";
   } else if (num < guess) {
     message.innerHTML = "No try a lower number";
-  } else {
+  } else if (num > guess) {
     message.innerHTML = "No try a higher number";
+  } else {
+    message.innerHTML = "That is not a number!";
   }
 }
 
