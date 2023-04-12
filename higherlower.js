87bb7d5631a2c49c06f3ebeb1ede8eca91c63056
Bryan W <bhoff1980@gmail.com>
2023-04-12 15:33:09 -0600

    added validation to the input

--- higherlower.js
+++ higherlower.js
@@ -1,18 +1,30 @@
-const guessAction = document.getElementById("guess-btn")
-let num = Math.floor(Math.random() * 20) +1;
+const guessAction = document.getElementById("guess-btn");
+const enteredNum = document.getElementById("entered-num");
+let choice = prompt("Please enter the largest number for the game");
+let isNumber = /^\d+$/.test(choice);
+
+let rounded = Math.round(choice);
+
+
+if (rounded < 1 || isNumber === false) {
+  alert("Please Enter a Positive Number");
+  document.location.reload();
+}
+enteredNum.innerHTML = rounded;
+
+let num = Math.floor(Math.random() * rounded) + 1;
 
 function do_guess() {
-    let guess = Number(document.getElementById("guess").value)
-    let message = document.getElementById("message")
+  let guess = Number(document.getElementById("guess").value);
+  let message = document.getElementById("message");
 
-    if (num === guess) {
-        message.innerHTML = "You got it!"
-
-    }else if (num < guess) {
-        message.innerHTML = "No try a lower number"
-    }else {
-        message.innerHTML = "No try a higher number"
-    }
+  if (num === guess) {
+    message.innerHTML = "You got it!";
+  } else if (num < guess) {
+    message.innerHTML = "No try a lower number";
+  } else {
+    message.innerHTML = "No try a higher number";
+  }
 }
 
-guessAction.addEventListener("click", do_guess)
+guessAction.addEventListener("click", do_guess);
